3d752f16a1a1df31c1d6d5aa22b4a51aae0f6eb4
Cândido da Silva Junior <57716228+candidosilva@users.noreply.github.com>
2024-09-29 11:06:59 -0300

    Added project

---
+++ components/Avatar.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="relative">
+    <nuxt-img
+      v-if="img"
+      :src="img"
+      class="h-10 w-10"
+      :class="[squareAvatar, borderAvatar]"
+    />
+
+    <div
+      v-else-if="initials"
+      class="h-10 w-10 bg-gray-100 flex justify-center items-center font-medium text-gray-600 text-xl"
+      :class="[squareAvatar, borderAvatar]"
+    >
+      {{ initials }}
+    </div>
+
+    <div
+      v-else
+      class="h-10 w-10 bg-gray-100"
+      :class="[squareAvatar, borderAvatar]"
+    >
+      <Icon name="mdi:account" size="40" color="gray" />
+    </div>
+
+    <span
+      v-if="dot"
+      class="absolute top-0 left-7 rounded-full border-2 border-white h-3.5 w-3.5 bg-red-400"
+      :class="`${square ? '-top-2 left-8' : 'top-0 left-7'}`"
+    ></span>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  img: {
+    type: String,
+  },
+  square: {
+    type: Boolean,
+  },
+  border: {
+    type: Boolean,
+  },
+  initials: {
+    type: String,
+  },
+  dot: {
+    type: Boolean,
+  },
+});
+
+const squareAvatar = computed(() => {
+  if (props.square) return "rounded";
+  return "rounded-full";
+});
+
+const borderAvatar = computed(() => {
+  if (props.border) return "ring-2 ring-blue-500";
+});
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mb-10 m-2 flex flex-col justify-center items-center">
+    <p>Default</p>
+    <Avatar img="woman.jpg" />
+
+    <p>Square</p>
+    <Avatar img="woman.jpg" square />
+
+    <p>Border</p>
+    <Avatar img="woman.jpg" border />
+    <Avatar img="woman.jpg" square border class="mt-2" />
+
+    <p>Icon</p>
+    <Avatar />
+    <Avatar square class="mt-2" />
+
+    <p>Initials</p>
+    <Avatar initials="CS" />
+    <Avatar initials="CS" square class="mt-2" />
+
+    <p>Dot</p>
+    <Avatar img="woman.jpg" dot />
+    <Avatar img="woman.jpg" dot square class="mt-2" />
+  </div>
+</template>
+
+<script setup></script>
